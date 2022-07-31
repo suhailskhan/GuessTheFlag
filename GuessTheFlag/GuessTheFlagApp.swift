77c021e6213b50a46bbb5c6c1e35f9d6274b9ffa
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-07-31 07:44:10 -0700

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Suhail Khan on 7/31/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
