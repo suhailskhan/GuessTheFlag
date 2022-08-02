d8f2d69eeadd1b9a5fb1143476e20e88da1d7564
Suhail Khan <26044674+suhailskhan@users.noreply.github.com>
2022-08-02 14:07:27 -0700

    Playing around

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,9 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: .center)
+            
+            Button {
+                showingAlert = true
+            } label: {
+                Label("Show Alert", systemImage: "info.circle")
+                    .foregroundStyle(RadialGradient(gradient: Gradient(colors: [.blue, .black]), center: .center, startRadius: 10, endRadius: 35))
+            }
+            .buttonStyle(.bordered)
+            .padding(50)
+            .background(.ultraThinMaterial)
+            .alert("Important message", isPresented: $showingAlert) {
+                Button("Delete", role: .destructive) { print("Deleted") }
+                Button("Cancel", role: .cancel) { print("Cancelled") }
+            }
+        }
+        .ignoresSafeArea()
     }
 }
 
